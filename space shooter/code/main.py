04a2e4312937bd6b40af258a715d73ed2ec13e90
Oberon <oberondsbw@gmail.com>
2025-04-14 20:35:47 +0100

    Add images, animation and relative paths

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -1,4 +1,5 @@
 import pygame
+from os.path import join
 
 # General set up
 pygame.init()
@@ -6,7 +7,16 @@ pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+pygame.display.set_caption('King of Space')
+
 running = True
+
+surface_main = pygame.Surface((100,200))
+surface_main.fill('yellow')
+xpos = 100
+
+# Import image
+player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
 
 while running:
     # Event loop
@@ -15,8 +25,9 @@ while running:
             running = False
 
     # Draw the game
-    display_surface.fill('red')
-    pygame.display.set_caption('HelloWorld!')
+    display_surface.fill('darkgray')
+    xpos += 0.25
+    display_surface.blit(player_surface,(xpos,150))
     pygame.display.update()
 
 pygame.quit()
