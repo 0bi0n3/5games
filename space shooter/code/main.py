f4a64c24e4ddd57a6e2a858839ead36b1564d793
Oberon <oberondsbw@gmail.com>
2025-04-14 20:13:17 +0100

    Add initial surface display, title and general set up

---
+++ space shooter/code/main.py
@@ -0,0 +1,22 @@
+import pygame
+
+# General set up
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+running = True
+
+while running:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Draw the game
+    display_surface.fill('red')
+    pygame.display.set_caption('HelloWorld!')
+    pygame.display.update()
+
+pygame.quit()
