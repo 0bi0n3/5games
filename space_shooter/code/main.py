b58e1540204e5f9f31b23c3701ab3ce99c7fc5b8
0bi0n3 <oberondaywest@gmail.com>
2025-05-17 19:05:03 +0100

    Update file paths, folder names, draw stars, reorganise drawing order

--- space_shooter/code/main.py
+++ space_shooter/code/main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from os.path import join
 
 # General set up
@@ -14,9 +15,14 @@ running = True
 surface_main = pygame.Surface((100,200))
 surface_main.fill('yellow')
 xpos = 100
+ypos = 200
 
-# Import image
-player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
+# Import images
+player_surface = pygame.image.load(join('..', 'images', 'player.png')).convert_alpha()
+stars_surface = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()
+
+star_positions = [(randint(0,WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
 
 while running:
     # Event loop
@@ -26,8 +32,15 @@ while running:
 
     # Draw the game
     display_surface.fill('darkgray')
-    xpos += 0.25
+    xpos += 0.75
+
+     # stars
+    for position in star_positions:
+        display_surface.blit(stars_surface, position)
+
+    # ship
     display_surface.blit(player_surface,(xpos,150))
+    
     pygame.display.update()
 
 pygame.quit()
